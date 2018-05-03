22450c349d154c72ef05e8453509c94cda7f691e
Gokhan Simsek <g.simsek@paralect.com>
2018-05-03 11:04:46 +0300

    Adding Refresh button and transaction info shown after voting

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Web3 from 'web3';
 
 // Note: Currently, these names are hardcoded in Ethereum contract
 const candidates = [0,1,2];
+const candidatesNames = ['Alex', 'Nick', 'Jose'];
 
 class App extends Component {
   
@@ -16,19 +17,30 @@ class App extends Component {
     const abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"uint8"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"uint8"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint8"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"uint8"}],"name":"uint8ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"uint8"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"uint8[]"}],"payable":false,"type":"constructor"}]');
     const votingContract = this.web3.eth.contract(abi);
     this.contractInstance = votingContract.at('0xBc3495943e8Bc4f95068355fbE5A456ed19e7FcD');
+  }
 
-    const votesCand1 = this.contractInstance.totalVotesFor.call(candidates[0]).toString();
-    const votesCand2 = this.contractInstance.totalVotesFor.call(candidates[1]).toString();
-    const votesCand3 = this.contractInstance.totalVotesFor.call(candidates[2]).toString();
-    
-    this.state = { 
-      votes: [votesCand1, votesCand2, votesCand3]
-    };
+  state = {
+    votes: [],
+    showTxInfo: false
+  }
+  
+  componentDidMount() {
+    this.updateVotes();
   }
 
   voteForCandidate = (index) => {
-    this.contractInstance.voteForCandidate(candidates[index], {from: this.web3.eth.accounts[0]});      
+    this.transactionHash = this.contractInstance.voteForCandidate(candidates[index], {from: this.web3.eth.accounts[0]});      
+    this.lastVotedName = candidatesNames[index];
+    this.setState({showTxInfo: true});
   }
+
+  updateVotes = () => {    
+    console.log("aaa");
+    const votesCand1 = this.contractInstance.totalVotesFor.call(candidates[0]).toString();
+    const votesCand2 = this.contractInstance.totalVotesFor.call(candidates[1]).toString();
+    const votesCand3 = this.contractInstance.totalVotesFor.call(candidates[2]).toString();    
+    this.setState({votes: [votesCand1, votesCand2, votesCand3]});    
+  } 
 
   render() {
 
@@ -36,9 +48,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simple Voting DApp with Ethereum</h1>
+          <h1 className="App-title">Voting DApp using Ethereum 'Rinkeby' Testnet</h1>
         </header>
-
+        
         <div style={{margin:'20px', textAlign:'left'}} class="table-responsive">
           <table class="table table-bordered">
             <thead>
@@ -49,35 +61,53 @@ class App extends Component {
             </thead>
             <tbody>
               <tr>
-                <td> Alex </td>
+                <td>                   
+                  <a href="#" onClick={() => this.voteForCandidate(0)} style={{margin:'0px'}}
+                              class="btn btn-primary pull-left"> 
+                    Vote Alex
+                  </a>                                
+                </td>
                 <td id="candidate-1"> {this.state.votes[0]} </td>
               </tr>
               <tr>
-                <td> Nick </td>
+                <td> 
+                  <a href="#" onClick={() => this.voteForCandidate(1)} style={{margin:'0px'}}
+                              class="btn btn-primary pull-left"> 
+                    Vote Nick
+                  </a>                    
+                </td>
                 <td id="candidate-2"> {this.state.votes[1]} </td>
               </tr>
               <tr>
-                <td> Jose </td>
+                <td> 
+                  <a href="#" onClick={() => this.voteForCandidate(2)} style={{margin:'0px'}}
+                              class="btn btn-primary pull-left"> 
+                    Vote Jose
+                  </a>                    
+                </td>
                 <td id="candidate-3"> {this.state.votes[2]} </td>
               </tr>
             </tbody>
           </table>
-
+          {
+            this.state.showTxInfo ?
+              <p>
+                Thanks for voting for {this.lastVotedName}. <br/>
+                Your votes has been submitted into Ethereum blockchain. 
+                Changes in Ethereum blockchain currently takes ~10-15 seconds to take affect. <br/>
+                <b>Transaction Details:</b> {' '}
+                  <a href={"https://rinkeby.etherscan.io/tx/" + this.transactionHash}>
+                    https://rinkeby.etherscan.io/tx/{this.transactionHash}
+                  </a>
+              </p>
+            : null
+          }  
           <div class="btn-group-vertical"  style={{width:'160px'}} >
-            <a href="#" onClick={() => this.voteForCandidate(0)} style={{margin:'0px'}}
-                        class="btn btn-primary pull-left"> 
-              Vote for Alex
-            </a>
-            <a href="#" onClick={() => this.voteForCandidate(1)} style={{marginTop:'10px'}}
-                        class="btn btn-primary pull-left"> 
-              Vote for Nick
-            </a>
-            <a href="#" onClick={() => this.voteForCandidate(2)} style={{marginTop:'10px'}}
-                        class="btn btn-primary pull-left"> 
-              Vote for Jose
-            </a>
+            <a href="#" onClick={this.updateVotes} style={{margin:'0px', backgroundColor: '#4dd2ff'}}
+                        class="btn btn-primary pull-center"> 
+              Refresh
+            </a>            
           </div>
-          
         </div>
       </div>
     );
